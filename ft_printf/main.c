caec55bda87ed35e00dffa7d93d2ce4a9b0f8adb
MLeganes <a.eme.ele@gmail.com>
2021-07-24 00:46:57 +0200

    Main, remove form libgit add main.c

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -10,20 +10,19 @@ int main()
 //int u;
 //	u = 0x23EE;
 	printf("\n -------- unsigned int ----------- \n");
-	printf("_%d_ ", ft_printf(" %x ", 10));
+	printf("_%d_ ", ft_printf(" %p ", (void *)-16));
 	printf("\n");
-    printf("_%d_", printf(" %x ", 10));    
+    printf("_%d_", printf(" %p ", (void *)-16));    
 	printf("\n");
 
 /***
- * HEXADECIMAL
- */
-	// int a;
-	// a = 0x0000ee;
-	// printf("\n");
-	// printf("%d", ft_printf(" My hex: %x ", a));
-	// printf("\n");
-    // printf("%d", printf(" Sys hex: %x ", a));    
+ * POINTE*
+//  */
 
+//     TEST(1, print(" %p ", -1));
+// 	TEST(2, print(" %p ", 1));
+// 	TEST(3, print(" %p ", 15));
+// 	TEST(4, print(" %p ", 16));
+// 	TEST(5, print(" %p ", 17));
     return 0;
 }
